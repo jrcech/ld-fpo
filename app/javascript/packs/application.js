db7c79ce8d2483d716ee4a47427af104a951f29b
Jiří Čech <jiricech94@gmail.com>
2021-01-13 20:08:12 +0100

    Add frontend styles

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,3 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -12,4 +7,8 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+import "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 import "controllers"

a954018acd486e42accc91601302f3151fa7a01d
Jiří Čech <jiricech94@gmail.com>
2021-01-16 23:17:24 +0100

    Validate user input

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -8,6 +8,8 @@ class HomepageController < ApplicationController
 
     if @query.to_i != 0 && @query.to_i.digits.size == 8
       @results = OpenDataCz.search_by_id(@query.to_i)
+    elsif @query.size < 4
+      flash[:error] = 'Zadejte alespoň 4 znaky'
     else
       @results = OpenDataCz.search(@query)
     end

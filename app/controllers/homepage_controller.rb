a39b139f471f1944ea42f5df81d5f575d0125d66
Jiří Čech <jiricech94@gmail.com>
2021-01-18 10:50:37 +0100

    Fix flash messages

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -5,13 +5,16 @@ class HomepageController < ApplicationController
 
   def search
     @query = params[:query]
+    flash.clear
 
     if @query.to_i != 0 && @query.to_i.digits.size == 8
       @results = OpenDataCz.search_by_id(@query.to_i)
+      flash[:notice] = 'Žádné výsledky nenalezeny. Upravte dotaz.' if @results.blank?
     elsif @query.size < 4
       flash[:error] = 'Zadejte alespoň 4 znaky'
     else
       @results = OpenDataCz.search(@query)
+      flash[:notice] = 'Žádné výsledky nenalezeny. Upravte dotaz.' if @results.blank?
     end
 
     render :index

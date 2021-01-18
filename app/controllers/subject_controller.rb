9daebfe31d78584564b52de217501811607fc0a9
Jiří Čech <jiricech94@gmail.com>
2021-01-18 09:20:15 +0100

    Fix subject controller for production

--- app/controllers/subject_controller.rb
+++ app/controllers/subject_controller.rb
@@ -7,22 +7,8 @@ class SubjectController < ApplicationController
     @subject = params[:id]
 
     @results = OpenDataCz.find(@subject, params[:name])
-    ap @results
-    ap @results.size
-
     @zivnost = OpenDataCz.list_zivnost(@subject)
-    ap @zivnost
-
     @address = OpenDataCz.address(@subject)
-    ap @address
-
     @sanctions = OpenDataCz.coi_sanctions(@subject)
-
-    # ap OpenDataCz.coi_check_dates(@subject).size
-    # ap OpenDataCz.coi_check_instruments(@subject).map { |check| check.instrument.to_s }.group_by(&:itself).map { |key, value| [key, value.size]  }
-    # ap OpenDataCz.coi_sanctions(@subject).map { |solution| solution.sanction.to_i }.sum
-    # ap OpenDataCz.coi_sanctions(@subject).map(&:sanction)
-    # ap OpenDataCz.coi_check_types(@subject).size
-    # ap OpenDataCz.coi_check_types(@subject).map { |solution| solution.type.to_s.split('/').last }.group_by(&:itself).map { |key, value| [key, value.size]  }
   end
 end
